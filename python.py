1acae518b13a0c61f855fe3deb26152a5b40c5ba
kimberlyhernandez <k-hernandez02@hotmail.com>
2025-01-16 15:04:30 -0600

    Remove repeated items from a list

--- python.py
+++ python.py
@@ -1,1 +1,19 @@
-print("hello world")
+numbers = [1, 2, 3, 4, 2, 3, 5, 6, 6]
+repetition = []
+
+for number in numbers:
+    if number not in repetition:
+        repetition.append(number)
+print(repetition)
+
+
+
+list_one = [4, 5, 7, 7, 9, 5, 4, 8]
+item = []
+
+for repeat in list_one:
+    if repeat not in item:
+        item.append(repeat)
+print (item)
+
+
